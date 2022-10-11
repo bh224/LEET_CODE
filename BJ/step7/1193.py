c2b82b981e938eb6d9fbc19619edb55e716e4539
bh224 <ericakim224@gmail.com>
2022-10-11 23:21:03 +0900

    221011

--- BJ/step7/1193.py
+++ BJ/step7/1193.py
@@ -2,9 +2,16 @@
 """
 분수찾기
 """
-cycle = 2
-list =  [1]
+N = int(input())
+cycle = 1
+num = N
+while num > cycle:
+  num -= cycle
+  cycle += 1
 
-for i in range(2, cycle+cycle):
-    print(i)
-print(list)
+if cycle % 2 == 0:
+  list = [ i for i in range(1, cycle+1)]
+  print(f"{list[num-1]}/{list[-num]}")
+else:
+  list = [ i for i in range(cycle, 0, -1)]
+  print(f"{list[num-1]}/{list[-num]}")
